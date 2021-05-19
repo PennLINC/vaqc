a6f4e8bed61a4898c63b54fc5ba223f9e6823617
Matt Cieslak <mattcieslak@gmail.com>
2021-05-19 12:27:54 -0400

    get started on fmriprep

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [
+    'matplotlib',
+    'dipy',
+    'numpy',
+    'nilearn'
+]
 
 setup_requirements = [ ]
 
@@ -30,7 +35,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Python package for preparing data to be viewed in the Vue App for QC",
+    description="Python package for preparing data to be viewed in the "
+                "Vue App for QC",
     entry_points={
         'console_scripts': [
             'vaqc=vaqc.cli:main',
@@ -40,13 +46,13 @@ setup(
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='vaqc',
+    keywords='MRI,QC,JavaScript',
     name='vaqc',
     packages=find_packages(include=['vaqc', 'vaqc.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/mattcieslak/vaqc',
+    url='https://github.com/pennlinc/vaqc',
     version='0.1.0',
     zip_safe=False,
 )
--- vaqc/cli.py
+++ vaqc/cli.py
@@ -1,17 +1,24 @@
 """Console script for vaqc."""
 import argparse
 import sys
+from pathlib import Path
+from .vaqc import process_fmriprep
 
 
 def main():
     """Console script for vaqc."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
+    parser.add_argument('derivatives_dir',
+                        type=Path,
+                        action='store',
+                        help='the root folder of a BIDS derivative dataset '
+                        '(sub-XXXXX folders '
+                        'should be found at the top level in this folder).')
     args = parser.parse_args()
+    print("using input directory as ", args.derivatives_dir)
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "vaqc.cli.main")
+    if args.derivatives_dir.name == 'fmriprep':
+        process_fmriprep(args.derivatives_dir)
     return 0
 
 
